3727a2ad98544abf1528859a4e159b8561ebead0
TMInstaller <bjdy3000@gmail.com>
2024-02-15 17:37:20 +0900

    rename: 파일 또는 폴더 명을 수정하거나 옮기는 작업만인 경우

---
+++ src/components/QuizLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center w-full h-[90vh] bg-none">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'QuizLayout',
+};
+</script>
