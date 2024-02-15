b2de8c477cd6b463eb27a83c1526ba1883f523af
TMInstaller <bjdy3000@gmail.com>
2024-02-15 17:36:44 +0900

    fix: router name 중복으로 인한 오류 수정

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,16 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import HelloWorld from '../components/HelloWorld.vue';
 import test from '../components/test.vue';
-import Auth from '../pages/Auth/AuthPage.vue'
-import QuizRoomPage from '../pages/quiz_room/QuizRoomPage.vue'
-import QuizGamePage from '../pages/quiz_game/QuizGamePage.vue'
-import QuizAttendpage from '../pages/quiz_attend/QuizAttendPage.vue'
-import QuizResultPage from '../pages/quiz_result/QuizResultPage.vue'
-import QuizCreatePage from '../pages/quiz_create/QuizCreatePage.vue'
-import Mypage from '../pages/my_page/Mypage.vue'
-import InfoPage from '../pages/info/InfoPage.vue'
-import NoticePage from '../pages/notice/NoticePage.vue'
-import Layout from '../components/Layout.vue'
+import Auth from '../pages/Auth/AuthPage.vue';
+import QuizLayout from '../components/QuizLayout.vue';
+import QuizGamePage from '../pages/quiz_game/QuizGamePage.vue';
+import QuizAttendpage from '../pages/quiz_attend/QuizAttendPage.vue';
+import QuizResultPage from '../pages/quiz_result/QuizResultPage.vue';
+import QuizCreatePage from '../pages/quiz_create/QuizCreatePage.vue';
+import QuizRoomPage from '../pages/quiz_room/QuizRoomPage.vue';
+import Mypage from '../pages/my_page/Mypage.vue';
+import InfoPage from '../pages/info/InfoPage.vue';
+import NoticePage from '../pages/notice/NoticePage.vue';
 
 const routes = [
   {
@@ -24,54 +24,55 @@ const routes = [
   {
     path: '/auth',
     name: 'Auth',
-    component: Auth
+    component: Auth,
   },
   {
-    path: "/quiz",
-    component: Layout,
-    children:[
+    path: '/quiz',
+    component: QuizLayout,
+    children: [
       {
-        path: "room",
-        name : 'quizroom',
-        component: QuizRoomPage
+        // 퀴즈 대기실
+        path: 'room',
+        name: 'quizroom',
+        component: QuizRoomPage,
       },
       {
-        path: "game",
-        name : 'quizgame',
-        component: QuizGamePage
+        path: 'game',
+        name: 'quizgame',
+        component: QuizGamePage,
       },
       {
-        path: "attend",
-        name : 'quizattend',
-        component: QuizAttendpage
+        path: 'attend',
+        name: 'quizattend',
+        component: QuizAttendpage,
       },
       {
-        path: "result",
-        name : 'quizroom',
-        component:  QuizResultPage
+        path: 'result',
+        name: 'quizresult',
+        component: QuizResultPage,
       },
       {
-        path: "create",
-        name : 'quizroom',
-        component: QuizCreatePage
+        path: 'create',
+        name: 'quizcreate',
+        component: QuizCreatePage,
       },
-    ]
+    ],
   },
   {
-    path: "/mypage",
+    path: '/mypage',
     name: 'mypage',
-    component: Mypage
+    component: Mypage,
   },
   {
-    path: "/info",
-    name : 'infomation',
-    component: InfoPage
+    path: '/info',
+    name: 'infomation',
+    component: InfoPage,
   },
   {
-    path: "/notice",
-    name : 'notice',
-    component: NoticePage
-  }
+    path: '/notice',
+    name: 'notice',
+    component: NoticePage,
+  },
 ];
 
 const router = createRouter({
