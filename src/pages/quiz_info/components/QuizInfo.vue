f454ef71ae46d8bd8b390802c3cbe7c3e104d098
TMInstaller <bjdy3000@gmail.com>
2024-02-16 14:03:36 +0900

    feat: Quiz 정보 페이지 1차 퍼블리싱 완료

---
+++ src/pages/quiz_info/components/QuizInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="p-4 bg-gray-100 rounded-lg">
+    <div class="flex flex-col items-center">
+      <h2 class="mt-4 text-2xl font-bold">{{ quizInfo.title }}</h2>
+      <img
+        :src="quizInfo.imageUrl"
+        alt="Quiz Image"
+        class="object-cover w-48 h-48 rounded-lg"
+      />
+      <p class="mt-1 text-sm">제작자: {{ quizInfo.makerName }}</p>
+      <p class="mt-4">{{ quizInfo.description }}</p>
+      <p class="mt-2">
+        질문 출처 유형: {{ quizInfo.questionSourceTypes.join(', ') }}
+      </p>
+      <p>답변 유형: {{ quizInfo.answerTypes.join(', ') }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { DefineComponent, PropType } from 'vue';
+
+export default {
+  name: 'QuizInfo',
+  props: {
+    quizInfo: {
+      type: Object as PropType<{
+        title: string;
+        makerName: string;
+        imageUrl: string;
+        description: string;
+        questionSourceTypes: string[];
+        answerTypes: string[];
+      }>,
+      required: true,
+    },
+  },
+};
+</script>
