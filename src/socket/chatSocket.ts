a76c1ce8bc253987d43f9e5179f8be3c893c4b8d
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-03-07 11:43:08 +0900

    feat : Websocket으로 변경

--- src/socket/chatSocket.ts
+++ src/socket/chatSocket.ts
@@ -1,6 +1,5 @@
 // socket/chatSocket.ts
 import * as Stomp from 'webstomp-client';
-import * as SockJS from 'sockjs-client/dist/sockjs';
 
 export default class ChatSocket {
   stompClient: Stomp.Client|null = null;
@@ -13,11 +12,11 @@ export default class ChatSocket {
 
   connect() {
     //SockJS => http:
-    //WebSocket => ws:
-        const serverURL = ' http://localhost:8080/ws'; // 실제 서버 주소로 변경
-    const socket = new SockJS(serverURL);
+    //WebSocket => ws
+    const serverURL = 'ws://localhost:8080/ws'; // 실제 서버 주소로 변경
+    const socket = new WebSocket(serverURL);
     console.log(socket)
-
+    
     this.stompClient = Stomp.over(socket);
     console.log(this.stompClient)
     console.log(`${this.quizRoomId}번 방으로 접속중...`)
@@ -25,10 +24,10 @@ export default class ChatSocket {
     this.stompClient.connect({},this.onConnected, this.onError);
   }
   onConnected(){
-    console.log("연결 시도 중")
+    console.log("연결 성공")
 
     this.connected = true;
-    console.log('Connected');
+
       // 서버로부터 메세지를 받는 부분
     this.stompClient?.subscribe(`/topic/quiz-room/${this.quizRoomId}/join`, message => {
         // 메세지 처리 로직
@@ -41,13 +40,14 @@ export default class ChatSocket {
     this.stompClient?.subscribe(`/topic/quiz-room/${this.quizRoomId}/start`, message => {
         // 페이지 이동 로직 작성
         // 참가자 정보 + 방 정보를 넘겨줘야함
+        // API에 있음
       console.log(JSON.parse(message.body));
     });
   }
   onError(){
     console.log('error')
   }
-  
+
   sendMessage(msg,quizRoomId:number) {
     if (this.stompClient && this.stompClient.connected) {
       const message = JSON.stringify(msg);
