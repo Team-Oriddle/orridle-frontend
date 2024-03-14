6f381895365d6462ce0552e30c388c11277856f5
JeongHunHui <deca3248@gmail.com>
2024-03-14 17:56:19 +0900

    feat: 로그인 버튼 클릭 시 로그인 api 실행 및 메인 페이지 리다이렉션 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <a href="/" class="text-lg font-bold">Logo</a>
       <!-- 네비게이션 링크 추가 위치 -->
       <div>
-        <a href="/login" class="px-4 py-2 rounded hover:bg-gray-100">로그인</a>
+        <a href="http://localhost:8080/api/v1/login/google?redirectEndPoint=%2F" class="px-4 py-2 rounded hover:bg-gray-100">로그인</a>
         <a href="/signup" class="px-4 py-2 rounded hover:bg-gray-100"
           >회원가입</a
         >
