271f700c9ccc8272da1867b048dbf6b43fe4fa30
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-03-20 16:19:14 +0900

    feat : 게임 시작 버튼 수정

--- src/pages/quiz_room/components/StartGameButton.vue
+++ src/pages/quiz_room/components/StartGameButton.vue
@@ -1,39 +1,41 @@
-<template>
-  <button
-    class="w-full h-full px-4 py-2 font-bold text-black transition duration-300 ease-in-out transform bg-gray-400 rounded hover:bg-gray-500 hover:-translate-y-1"
-    @click="startGame(roomId)"
-  >
-    게임 시작
-  </button>
-</template>
+  <template>
+    <button
+      class="w-full h-full px-4 py-2 font-bold text-black transition duration-300 ease-in-out transform bg-gray-400 rounded hover:bg-gray-500 hover:-translate-y-1"
+      @click="startGame(roomId)"
+    >
+      게임 시작
+    </button>
+  </template>
 
-<script lang="ts">
-import axios from 'axios';
-import { defineComponent } from 'vue';
+  <script lang="ts">
+  import axios from 'axios';
+  import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'StartGameButton',
-  props:{
-    roomId:{
-      type: Number,
-      required: true,
-    }
-  },
-  emits: ['click'],
-  methods: {
-    startGame:async(quizRoomId:number) =>{
-      try {
-        const response = await axios.post(`http://localhost:8080/api/v1/quiz-room/${quizRoomId}/start`,{
-          withCredentials: true,
-          headers:{
-            'Content-Type': 'application/json'
-          }
-        })
-        console.log(response)
-      } catch (error) {
-        console.error(error)
+  export default defineComponent({
+    name: 'StartGameButton',
+    props:{
+      roomId:{
+        type: Number,
+        required: true,
       }
-    }
-  },
-});
-</script>
+    },
+    emits: ['click'],
+    methods: {
+      startGame:async(quizRoomId:number) =>{
+        try {
+          const response = await axios.post(`http://localhost:8080/api/v1/quiz-room/${quizRoomId}/start`,
+          {},
+          {
+            withCredentials: true,
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          })
+          console.log(response)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    },
+  });
+  </script>
